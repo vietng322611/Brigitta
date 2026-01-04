9bca3ae18a4f09f6dc83ed6bff3b36bdb355c899
vietng322611 <vietng322611@gmail.com>
2026-01-05 02:17:57 +0700

    feat: add remember password

---
+++ BrigittaBlazor/Utils/PasswordUtils.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace BrigittaBlazor.Utils;
+
+public static class PasswordUtils
+{
+    public static string Encrypt(string password)
+    {
+        var provider = DataProtectionProvider.Create(
+            new DirectoryInfo("keys")
+        );
+        var protector = provider.CreateProtector("AutoLogin.Password.v1");
+        
+        return Convert.ToBase64String(
+            protector.Protect(Encoding.UTF8.GetBytes(password))
+        );
+    }
+
+    public static string Decrypt(string protectedPassword)
+    {
+        var provider = DataProtectionProvider.Create(
+            new DirectoryInfo("keys")
+        );
+        var protector = provider.CreateProtector("AutoLogin.Password.v1");
+        
+        return Encoding.UTF8.GetString(
+            protector.Unprotect(Convert.FromBase64String(protectedPassword))
+        );
+    }
+}
