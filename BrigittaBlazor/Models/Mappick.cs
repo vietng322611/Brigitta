3d758ba00b771268ff31848f58da7ad1e389858a
vietng322611 <vietng322611@gmail.com>
2026-01-09 18:51:56 +0700

    feat: add map selection panel + `Mappick` model + mappool utilities

---
+++ BrigittaBlazor/Models/Mappick.cs
@@ -0,0 +1,31 @@
+﻿using MudBlazor;
+
+namespace BrigittaBlazor.Models;
+
+public class Mappick
+{
+    public string Abbreviation { get; set; } = "";
+    public int ModComb { get; set; }
+    public int MapId { get; set; }
+
+    public Mappick() { }
+
+    public Mappick(string abbreviation, int modComb, int mapId)
+    {
+        Abbreviation = abbreviation;
+        ModComb = modComb;
+        MapId = mapId;
+    }
+
+    public Color GetColor()
+        => (Abbreviation[..2]) switch 
+        { 
+            "NM" => Color.Info,
+            "HD" => Color.Warning,
+            "HR" => Color.Error,
+            "DT" => Color.Primary,
+            "FM" => Color.Success,
+            "TB" => Color.Warning,
+            _ => Color.Info
+        };
+}
---
+++ BrigittaBlazor/Utils/MappoolUtils.cs
@@ -0,0 +1,36 @@
+﻿using BrigittaBlazor.Models;
+
+namespace BrigittaBlazor.Utils;
+
+public static class MappoolUtils
+{
+    /*
+     * Return a sorted List<Mappick> in following order:
+     * NM, HD, HR, DT, FM, ...custom picks, TB
+     */
+    public static List<Mappick>? LoadMappool(string tourAbbr)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static List<Mappick>? LoadMappool()
+    {
+        return
+        [
+            new Mappick("NM1", 1, 123456),
+            new Mappick("NM2", 1, 123456),
+            new Mappick("NM3", 1, 123456),
+            new Mappick("NM4", 1, 123456),
+            new Mappick("NM5", 1, 123456),
+            new Mappick("HD1", 9, 123456),
+            new Mappick("HD2", 9, 123456),
+            new Mappick("HR1", 17, 123456),
+            new Mappick("HR2", 17, 123456),
+            new Mappick("DT1", 65, 123456),
+            new Mappick("DT2", 65, 123456),
+            new Mappick("DT3", 65, 123456),
+            new Mappick("FM1", -1, 123456),
+            new Mappick("TB", -1, 123456)
+        ];
+    }
+}
